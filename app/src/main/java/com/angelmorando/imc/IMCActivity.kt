aa019ecdfe3395a28c54cf242f1e75b628f9b77f
amorgal023 <amorgal023@g.educaand.es>
2023-12-06 16:59:32 +0100

    Initial commit

---
+++ app/src/main/java/com/angelmorando/imc/IMCActivity.kt
@@ -0,0 +1,11 @@
+package com.angelmorando.imc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IMCActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imcactivity)
+    }
+}
